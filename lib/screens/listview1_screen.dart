2a691ca9503d00d02765241e41abc9d17a054e92
Nahuel Castro <nahu.castro208@gmail.com>
2022-07-18 16:51:19 -0300

    Initial commit

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
+
+  const Listview1Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Listview Tipo 1',
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          ...options
+              .map<Widget>(
+                (game) => ListTile(
+                  title: Text(game),
+                  trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                ),
+              )
+              .toList(),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy',
+    'Contra'
+  ];
+
+  const Listview2Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Listview Tipo 2',
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.gamepad_outlined),
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios_outlined),
+          // separatorBuilder: (context, index) => const Divider(),
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+      ),
+    );
+  }
+}
