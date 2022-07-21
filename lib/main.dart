469da60022cc7277922b539bf8bfbc874bf3beec
Nahuel Castro <nahu.castro208@gmail.com>
2022-07-21 20:34:28 -0300

    ROUTES

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:fl_components/screens/screens.dart';
+import 'router/app_routes.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,20 +13,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       // home: const Listview2Screen(),
-      initialRoute: 'home',
-      routes: {
-        'home': (BuildContext context) => const HomeScreen(),
-        'listview1': (BuildContext context) => const Listview1Screen(),
-        'listview2': (BuildContext context) => const Listview2Screen(),
-        'alert': (BuildContext context) => const AlertScreen(),
-        'card': (BuildContext context) => const CardScreen(),
-      },
-      onGenerateRoute: (settings) {
-        // print(settings);
-        return MaterialPageRoute(
-          builder: (context) => const AlertScreen(),
-        );
-      },
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
     );
   }
 }
---
+++ lib/router/app_routes.dart
@@ -0,0 +1,50 @@
+import 'package:fl_components/Models/models.dart';
+import 'package:flutter/material.dart';
+import 'package:fl_components/screens/screens.dart';
+
+class AppRoutes {
+  static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+        route: 'home',
+        name: 'Home Screen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOption(
+        route: 'listview1',
+        name: 'Listview Tipo 1',
+        screen: const Listview1Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'listview2',
+        name: 'Listview Tipo 2',
+        screen: const Listview2Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'alert',
+        name: 'Alertas ',
+        screen: const AlertScreen(),
+        icon: Icons.add_alert_rounded),
+    MenuOption(
+        route: 'card',
+        name: 'Tarjetas',
+        screen: const CardScreen(),
+        icon: Icons.credit_card),
+  ];
+
+  static Map<String, Widget Function(BuildContext)> routes = {
+    'home': (BuildContext context) => const HomeScreen(),
+    'listview1': (BuildContext context) => const Listview1Screen(),
+    'listview2': (BuildContext context) => const Listview2Screen(),
+    'alert': (BuildContext context) => const AlertScreen(),
+    'card': (BuildContext context) => const CardScreen(),
+  };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    // print(settings);
+    return MaterialPageRoute(
+      builder: (context) => const AlertScreen(),
+    );
+  }
+}
