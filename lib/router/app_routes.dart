b71f5c22b8a991c8d03bb6df9c91c7e46b6b3ffd
Nahuel Castro <nahu.castro208@gmail.com>
2022-07-25 21:05:12 -0300

    sacar el homescreen como options

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,11 +6,11 @@ class AppRoutes {
   static const initialRoute = 'home';
 
   static final menuOptions = <MenuOption>[
-    MenuOption(
-        route: 'home',
-        name: 'Home Screen',
-        screen: const HomeScreen(),
-        icon: Icons.home),
+    // MenuOption(
+    //     route: 'home',
+    //     name: 'Home Screen',
+    //     screen: const HomeScreen(),
+    //     icon: Icons.home),
     MenuOption(
         route: 'listview1',
         name: 'Listview Tipo 1',
@@ -45,15 +45,13 @@ class AppRoutes {
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+    appRoutes.addAll({'home': (BuildContext context) => const HomeScreen()});
 
     for (final option in menuOptions) {
       appRoutes.addAll({option.route: (BuildContext context) => option.screen});
     }
 
-    return {
-      for (final option in menuOptions)
-        option.route: (context) => option.screen,
-    };
+    return appRoutes;
   }
   // static Map<String, Widget Function(BuildContext)> routes = {
   //   'home': (BuildContext context) => const HomeScreen(),
