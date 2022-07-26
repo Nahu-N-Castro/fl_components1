e154e8b8839ac4039b0f8e05c0bbb0c8c89d085d
Nahuel Castro <nahu.castro208@gmail.com>
2022-07-25 21:41:11 -0300

    inputs

--- lib/screens/inputs_screens.dart
+++ lib/screens/inputs_screens.dart
@@ -16,15 +16,33 @@ class InputsScreen extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
-                autofocus: true,
+                autofocus: false,
                 initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) => debugPrint('value:$value'),
-                decoration: const InputDecoration(
+                validator: (value) {
+                  if (value == null) return 'Campo obligatorio';
+                  return value.length < 3 ? 'Mínimo 3 caracteres' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
                   labelText: 'Nombre',
                   hintText: 'Ingrese su nombre',
-                  prefixIcon: Icon(Icons.person, color: Colors.red),
-                  border: OutlineInputBorder(),
+                  helperText: 'Solo letras',
+                  // counterText: '3 Caracteres',
+                  // suffixIcon: Icon(Icons.group_outlined, color: Colors.red),
+                  prefixIcon: const Icon(Icons.person, color: Colors.red),
+                  icon: const Icon(Icons.assignment_ind_outlined),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.red),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
                 ),
               ),
             ],
